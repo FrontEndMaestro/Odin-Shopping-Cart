6e6b1ecb4e26ee5a9a75af4d92f713c916242bf7
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-05-12 15:47:20 +0500

    feat: add icons and their respective styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router";
 import HomeIcon from "@mui/icons-material/Home";
 import style from "../styles/nav.module.css";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 export default function Nav() {
   return (
     <div className={style.container}>
-      <Link to="/">{HomeIcon}</Link>
-      <Link to="/shop" className={style.shop}>
-        Shop
+      <Link to="/" className={`${style.icon}`}>
+        <HomeIcon className={`${style.svg}`} />
       </Link>
-      <Link to="/cart">Cart</Link>
+      <p className={`${style.name}`}>Urban Threads.</p>
+      <Link to="/shop" className={`${style.icon} ${style.shop}`}>
+        <ShoppingBagIcon className={`${style.svg}`} />
+      </Link>
+      <Link to="/cart" className={`${style.icon}`}>
+        <ShoppingCartIcon className={`${style.svg}`} />
+      </Link>
     </div>
   );
 }
