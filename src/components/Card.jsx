616f97fd677006f6a5800dcc67913ea5707d718c
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-05-15 02:03:32 +0500

    feat: add card component for product listing

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,34 @@
-export default function Card({ imgUrl, title, description }) {
+import style from "../styles/card.module.css";
+import { useState } from "react";
+export default function Card({ imgUrl, title, price }) {
+  const [count, setCount] = useState(0);
+  function decrement() {
+    if (count != 0) setCount(count - 1);
+  }
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function changeQuantity(event) {
+    count = event.target.value;
+  }
   return (
-    <div>
+    <article className={`${style.card}`}>
       <img src={imgUrl} alt="" />
-      {title}
-    </div>
+      <div className={`${style.container}`}>
+        <h3>{title}</h3>
+        <p>${price}</p>
+        <div className={`${style.quantity_container}`}>
+          <button onClick={() => decrement()}>-</button>
+          <input
+            type="number"
+            Value={count}
+            onChange={() => changeQuantity()}
+          />
+          <button onClick={() => increment()}>+</button>
+        </div>
+      </div>
+    </article>
   );
 }
