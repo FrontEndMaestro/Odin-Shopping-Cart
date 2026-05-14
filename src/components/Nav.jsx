a7c09b5e4f5bbce3018432daada1ab3f7669acc2
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-05-14 16:07:04 +0500

    refactor: change tag to nav for accessibility

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,17 +5,17 @@ import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 export default function Nav() {
   return (
-    <div className={style.container}>
+    <nav className={style.container}>
       <Link to="/" className={`${style.icon}`}>
         <HomeIcon className={`${style.svg}`} />
       </Link>
-      <p className={`${style.name}`}>Urban Threads.</p>
+      <h1 className={`${style.name}`}>Urban Threads.</h1>
       <Link to="/shop" className={`${style.icon} ${style.shop}`}>
         <ShoppingBagIcon className={`${style.svg}`} />
       </Link>
       <Link to="/cart" className={`${style.icon}`}>
         <ShoppingCartIcon className={`${style.svg}`} />
       </Link>
-    </div>
+    </nav>
   );
 }
