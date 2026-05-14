e9329e4455744410d9ef8371315b70d14db3a55a
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-05-14 16:14:21 +0500

    feat: implement hero banner and side content
    Changed elements from div to main and added aside tag for side content.
    This was done for accessibility reasons.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import "./App.css";
 import { useState, useEffect } from "react";
 import { Link } from "react-router";
 import { Outlet } from "react-router";
+import heroImage from "./assets/Hero-Banner.jpg";
+import jewellaryImage from "./assets/Jewellary.jpg";
+import electronicsImage from "./assets/electronics.jpg";
+import womensClothingImage from "./assets/womens-clothing.jpg";
+import style from "./App.module.css";
+import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
 const useItemData = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -40,8 +45,40 @@ export default function App() {
   return (
     <>
       <Outlet />
-      <div>HOME</div>
-      <Link to="shop">Btn</Link>
+      <main className={`${style.main_container}`}>
+        <div className={`${style.hero_banner}`}>
+          <div className={`${style.hero_content}`}>
+            <h2>Upgrade Your Wardrobe with Immaculate Quality</h2>
+            <h3>
+              Discover clothing designed with refined style, lasting comfort,
+              and premium attention to detail. Each piece is made to elevate
+              your everyday look with confidence and ease.
+            </h3>
+            <Link to="/shop" className={`${style.shopping_link}`}>
+              View All Products
+              <div>
+                <ArrowOutwardIcon />
+              </div>
+            </Link>
+          </div>
+
+          <img src={heroImage} alt="" />
+        </div>
+        <aside className={`${style.side_container}`}>
+          <div className={`${style.card}`}>
+            <img src={electronicsImage} alt="" />
+            <h2>Electronics</h2>
+          </div>
+          <div className={`${style.card}`}>
+            <img src={womensClothingImage} alt="" />
+            <h2>Explore our Women's clothing range.</h2>
+          </div>
+          <div className={`${style.card}`}>
+            <img src={jewellaryImage} alt="" />
+            <h2>Jewellary</h2>
+          </div>
+        </aside>
+      </main>
     </>
   );
 }
