0be8e7465b46ac522c81b6855e0b340e9370800d
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-05-16 17:53:33 +0500

    feat: implement item quantity adjustments
    Allows users to change product quantities and introduces an add-to-cart
    button on product cards. Modifying the quantity updates the quantity of
    the specific product in data array in App.jsx component.
    
    Added styling implementing flexbox layouts and smooth transition for
    enhanced user experience.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,47 @@
 import style from "../styles/card.module.css";
 import { useState } from "react";
-export default function Card({ imgUrl, title, price }) {
+export default function Card({ id, imgUrl, title, price, editCartCount }) {
   const [count, setCount] = useState(0);
   function decrement() {
-    if (count != 0) setCount(count - 1);
+    if (count != 0) {
+      setCount((count) => count - 1);
+      editCartCount(id, count);
+    }
   }
 
   function increment() {
-    setCount(count + 1);
+    setCount((count) => count + 1);
+    editCartCount(id, count);
   }
 
   function changeQuantity(event) {
-    count = event.target.value;
+    console.log(event);
+    setCount(Number(event.target.value));
   }
+
+  function addToCart() {}
+
   return (
-    <article className={`${style.card}`}>
+    <article className={style.card}>
       <img src={imgUrl} alt="" />
-      <div className={`${style.container}`}>
+      <div className={style.container}>
         <h3>{title}</h3>
         <p>${price}</p>
-        <div className={`${style.quantity_container}`}>
+        <div className={style["quantity-container"]}>
           <button onClick={() => decrement()}>-</button>
           <input
             type="number"
             Value={count}
-            onChange={() => changeQuantity()}
+            onChange={(event) => changeQuantity(event)}
           />
           <button onClick={() => increment()}>+</button>
         </div>
+        <button
+          className={style["add-to-cart-btn"]}
+          onClick={() => addToCart()}
+        >
+          Add to Cart
+        </button>
       </div>
     </article>
   );
