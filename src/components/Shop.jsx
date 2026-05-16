3a47e201c31b73b6a280b02d7ac8e978e325e547
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-05-16 18:00:26 +0500

    feat: create product card components
    Renders a dedicated card component for each item in the product data
    array to display its details dynamically.
    
    Call useOutletContext to pass down the data array
    and quantity update function, enabling each product card to update the
    products count.
    
    Adds a new stylesheet utilizing CSS Grid for a clean product layout.

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,9 +1,21 @@
-import { Outlet } from "react-router";
+import { Outlet, useOutletContext } from "react-router";
+import style from "../styles/shop.module.css";
+import Card from "./card";
 export default function Shop() {
+  const { data, editCartCount } = useOutletContext();
   return (
     <>
-      <Outlet />
-      <div>Shop</div>;
+      <div className={`${style.container}`}>
+        {data.map((element) => (
+          <Card
+            id={element.id}
+            imgUrl={element.image}
+            title={element.title}
+            price={element.price}
+            editCartCount={editCartCount}
+          ></Card>
+        ))}
+      </div>
     </>
   );
 }
