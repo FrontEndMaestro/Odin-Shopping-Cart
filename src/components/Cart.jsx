b875477d877383fd78c9f6b50677d311f2f7a6e2
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-05-18 14:14:30 +0500

    feat(cart): add cart component
    Render selected items using the base Card component and provide core
    shopping cart functionalities.
    
    Users can now:
    - Adjust item quantities
    - Remove items entirely from the cart
    - View total item count and price summaries

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,42 @@
-import { Outlet } from "react-router";
+import style from "../styles/cart.module.css";
+import Card from "./Card.jsx";
+import { useOutletContext } from "react-router";
 export default function Cart() {
-  return  <>
-      <Outlet />
-      <div>CART</div>;
-    </>;
+  const { data, editCartCount, totalCount, totalPrice } = useOutletContext();
+  return (
+    <>
+      <h1 className={style.heading}>Your Bag</h1>
+      {totalCount != 0 ? (
+        <div className={`${style.container}`}>
+          <div className={style["card-container"]}>
+            {data.map((element) =>
+              element.cartCount != 0 ? (
+                <Card
+                  id={element.id}
+                  imgUrl={element.image}
+                  title={element.title}
+                  price={element.price}
+                  editCartCount={editCartCount}
+                  type="cart"
+                  quantityInCart={element.cartCount}
+                ></Card>
+              ) : null,
+            )}
+          </div>
+          <div className={style["order-summary"]}>
+            <h2>Order Summary</h2>
+            <hr />
+            <div className={style["totalcount-display"]}>
+              <p>Items</p> <p>{totalCount}</p>
+            </div>
+            <div className={style["total-price-display"]}>
+              <p>Total</p> <p>${Math.round(totalPrice)}</p>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <h2>No Items in cart</h2>
+      )}
+    </>
+  );
 }
